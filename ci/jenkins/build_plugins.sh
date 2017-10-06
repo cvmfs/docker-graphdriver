0969b2400bb505d5028ebacb835f99849a00cb43
Jakob Blomer <jblomer@cern.ch>
2017-10-06 14:55:40 +0200

    add Jenkins build script

---
+++ ci/jenkins/build_plugins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+die() {
+  echo "$1"
+  exit 1
+}
+
+[ "x$GOROOT" != x ] || die "GOROOT missing"
+[ "x$WORKSPACE" != x ] || die "WORKSPACE missing"
+[ "x$CVMFS_SOURCE_LOCATION" != x ] || die "CVMFS_SOURCE_LOCATION missing"
+[ "x$CVMFS_BUILD_LOCATION" != x ] || die "CVMFS_BUILD_LOCATION missing"
+
+export GOPATH="$WORKSPACE"
+PLUGINS_ROOT="github.com/cvmfs/docker-graphdriver/plugins"
+
+cd $CVMFS_BUILD_LOCATION
+for plugin in aufs_cvmfs overlay2_cvmfs; do
+  echo "Building: $plugin"
+  PATH=$GOROOT/bin:$PATH go build -v "$PLUGINS_ROOT/$plugin"
+done
+
