6652b155626e82d657c25446b84c2a447e240a52
Jakob Blomer <jblomer@cern.ch>
2017-10-06 17:43:35 +0200

    create versioned build products

--- ci/jenkins/build_plugins.sh
+++ ci/jenkins/build_plugins.sh
@@ -20,8 +20,11 @@ for plugin in aufs_cvmfs overlay2_cvmfs; do
   echo "Building: $plugin"
 
   VERSION=$(cat $CVMFS_SOURCE_LOCATION/$PLUGINS_ROOT/$plugin/VERSION)
+  mkdir -p $plugin/$VERSION
+  pushd $plugin/$VERSION
   PATH=$GOROOT/bin:$PATH go build \
     -ldflags="-X main.version=$VERSION -X main.git_hash=$GIT_COMMIT" \
     -v "$PLUGINS_ROOT/$plugin"
+  popd
 done
 
