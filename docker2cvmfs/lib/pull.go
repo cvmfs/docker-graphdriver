927e3bc0f7cf7dd261ad1d0e2f3c5619e88ede99
Jakob Blomer <jblomer@cern.ch>
2017-11-02 11:06:20 +0100

    allow for variable destination layer when pulling images

--- docker2cvmfs/lib/pull.go
+++ docker2cvmfs/lib/pull.go
@@ -9,25 +9,29 @@ import (
 )
 
 func PullLayers(dockerRegistryUrl string, args []string) {
-	if len(args) != 1 {
+	if len(args) < 1 {
 		printUsage()
 		return
 	}
 
 	reference := args[0]
 	manifest := getManifest(dockerRegistryUrl, reference)
-
 	image := strings.Split(reference, ":")[0]
 
-	os.Mkdir("/tmp/layers", 0755)
+	destDir := "/tmp/layers"
+	if len(args) > 1 {
+		destDir = args[1]
+	}
+
+	os.Mkdir(destDir, 0755)
 	for idx, layer := range manifest.Layers {
 		fmt.Printf("%2d: %s\n", idx, layer.Digest)
-		getLayer(dockerRegistryUrl, image, layer.Digest)
+		getLayer(dockerRegistryUrl, image, layer.Digest, destDir)
 	}
 }
 
-func getLayer(dockerRegistryUrl, repo, digest string) {
-	filename := "/tmp/layers/" + strings.Split(digest, ":")[1] + ".tar.gz"
+func getLayer(dockerRegistryUrl, repo, digest string, destDir string) {
+	filename := destDir + "/" + strings.Split(digest, ":")[1] + ".tar.gz"
 
 	file, _ := os.Create(filename)
 	fmt.Println(filename)
