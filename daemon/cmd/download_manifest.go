430f09ff63601899f3f36acc68609604a0f9d8b7
Simone Mosciatti <simone.mosciatti@cern.ch>
2018-10-09 15:21:10 +0200

    remove useless functions

--- daemon/cmd/download_manifest.go
+++ daemon/cmd/download_manifest.go
@@ -3,9 +3,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
-	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -50,68 +48,4 @@ var downloadManifestCmd = &cobra.Command{
 		}
 		fmt.Println(string(text))
 	},
-}
-
-func parseBearerToken(token string) (realm string, options map[string]string, err error) {
-	options = make(map[string]string)
-	args := token[7:]
-	keyValue := strings.Split(args, ",")
-	for _, kv := range keyValue {
-		splitted := strings.Split(kv, "=")
-		if len(splitted) != 2 {
-			err = fmt.Errorf("Wrong formatting of the token")
-			return
-		}
-		splitted[1] = strings.Trim(splitted[1], `"`)
-		if splitted[0] == "realm" {
-			realm = splitted[1]
-		} else {
-			options[splitted[0]] = splitted[1]
-		}
-	}
-	return
-}
-
-func requestAuthToken(token string) (authToken string, err error) {
-	realm, options, err := parseBearerToken(token)
-	if err != nil {
-		return
-	}
-	req, err := http.NewRequest("GET", realm, nil)
-	if err != nil {
-		return
-	}
-
-	query := req.URL.Query()
-	for k, v := range options {
-		query.Add(k, v)
-	}
-	req.URL.RawQuery = query.Encode()
-
-	if username != "" || pass != "" {
-		req.SetBasicAuth(username, pass)
-	}
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	defer resp.Body.Close()
-
-	if resp.StatusCode >= 400 {
-		err = fmt.Errorf("Authorization error %s", resp.Status)
-		return
-	}
-
-	var jsonResp map[string]interface{}
-	err = json.NewDecoder(resp.Body).Decode(&jsonResp)
-	if err != nil {
-		return
-	}
-	authTokenInterface, ok := jsonResp["token"]
-	if ok {
-		authToken = "Bearer " + authTokenInterface.(string)
-		return
-	} else {
-		err = fmt.Errorf("Didn't get the token key from the server")
-	}
-	return
 }
