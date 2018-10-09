aa995f990c9f1771ea3373c5ad10921410537e1c
Simone Mosciatti <simone.mosciatti@cern.ch>
2018-10-09 15:21:48 +0200

    remove useless functions

--- daemon/lib/conversion.go
+++ daemon/lib/conversion.go
@@ -16,7 +16,6 @@ import (
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
-	"github.com/go-errors/errors"
 	log "github.com/sirupsen/logrus"
 
 	d2c "github.com/cvmfs/docker-graphdriver/docker2cvmfs/lib"
@@ -255,56 +254,4 @@ func ConvertWish(wish WishFriendly, convertAgain, forceDownload bool) (err error
 		Log().Warn("Some error during the conversion, we are not storing it into the database")
 		return
 	}
-}
-
-// copyBuffer is the actual implementation of Copy and CopyBuffer.
-// if buf is nil, one is allocated.
-func copyBuffer(dst io.Writer, src io.Reader, buf []byte) (written int64, err error) {
-	// If the reader has a WriteTo method, use it to do the copy.
-	// Avoids an allocation and a copy.
-	if wt, ok := src.(io.WriterTo); ok {
-		return wt.WriteTo(dst)
-	}
-	// Similarly, if the writer has a ReadFrom method, use it to do the copy.
-	if rt, ok := dst.(io.ReaderFrom); ok {
-		return rt.ReadFrom(src)
-	}
-	size := 32 * 1024
-	if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
-		if l.N < 1 {
-			size = 1
-		} else {
-			size = int(l.N)
-		}
-	}
-	if buf == nil {
-		buf = make([]byte, size)
-	}
-	for {
-		nr, er := src.Read(buf)
-		if nr > 0 {
-			nw, ew := dst.Write(buf[0:nr])
-			if nw > 0 {
-				written += int64(nw)
-			}
-			if ew != nil {
-				LogE(ew).Error("Write error")
-				err = ew
-				break
-			}
-			if nr != nw {
-				err = io.ErrShortWrite
-				break
-			}
-		}
-		if er != nil {
-			if er != io.EOF {
-				err = er
-				LogE(err).Error("Read error")
-				fmt.Println(err.(*errors.Error).ErrorStack())
-			}
-			break
-		}
-	}
-	return written, err
 }
--- daemon/main.go
+++ daemon/main.go
@@ -1,57 +1,11 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"strings"
-
 	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/cvmfs/docker-graphdriver/daemon/cmd"
 )
 
-var (
-	InsertDesiderata *sql.Stmt
-)
-
-/*
-
-Three simple commands:
-	1. add desiderata
-	2. refresh
-	3. check
-
-add desiderata will add a new row to the desiderata table and will try to convert it.
-refresh will check all the converted and be sure that they are up to date, maybe we changed a tag
-check will simply make sure that every desiderata is been converted
-
-We will talk with the docker daemon and use it for most of the work
-
-*/
-
-var (
-	username_input, password_input,
-	input_repository,
-
-	username_output, password_output,
-	output_repository,
-	cvmfs_repository string
-
-	machineFriendly bool
-)
-
 func main() {
 	cmd.EntryPoint()
-}
-
-func SplitReference(repository string) (register, tag, digest string, err error) {
-	tryTag := strings.Split(repository, ":")
-	if len(tryTag) == 2 {
-		return tryTag[0], tryTag[1], "", nil
-	}
-	tryDigest := strings.Split(repository, "@")
-	if len(tryDigest) == 2 {
-		return tryTag[0], "", tryTag[1], nil
-	}
-	return "", "", "", fmt.Errorf("Impossible to split the repository into repository and tag")
 }
