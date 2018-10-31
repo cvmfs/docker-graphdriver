6221e454cd2c49d5b7fa15e52670d59927dcaf93
Simone Mosciatti <simone.mosciatti@cern.ch>
2018-10-31 14:52:37 +0100

    remove ls-wish and PrintMultipleWishes function

--- daemon/lib/wish.go
+++ daemon/lib/wish.go
@@ -3,10 +3,6 @@ package lib
 import (
 	"database/sql"
 	"fmt"
-	"os"
-	"strconv"
-
-	"github.com/olekukonko/tablewriter"
 )
 
 type Wish struct {
@@ -34,33 +30,6 @@ type WishAlreadyInDBError struct{}
 
 func (e *WishAlreadyInDBError) Error() string {
 	return "Wish is already in the database"
-}
-
-func PrintMultipleWishes(wish []WishFriendly, machineFriendly, printHeader bool) {
-	if machineFriendly {
-		if printHeader {
-			fmt.Println("id,input_image_id,input_image_name,cvmfs_repo,output_image_id,output_image_name,converted")
-		}
-		for _, d := range wish {
-			fmt.Printf("%d,%d,%s,%s,%d,%s,%t\n", d.Id, d.InputId, d.InputName, d.CvmfsRepo, d.OutputId, d.OutputName, d.Converted)
-		}
-	} else {
-		table := tablewriter.NewWriter(os.Stdout)
-		table.SetAlignment(tablewriter.ALIGN_LEFT)
-		if printHeader {
-
-			table.SetHeader([]string{"Id", "Input Id",
-				"Input Image Name", "CVMFS Repo",
-				"Output Id", "Output Image Name", "Converted"})
-
-		}
-		for _, d := range wish {
-			table.Append([]string{strconv.Itoa(d.Id), strconv.Itoa(d.InputId),
-				d.InputName, d.CvmfsRepo,
-				strconv.Itoa(d.OutputId), d.OutputName, strconv.FormatBool(d.Converted)})
-		}
-		table.Render()
-	}
 }
 
 func CreateWish(inputImage, outputImage, cvmfsRepo, userInput, userOutput string) (wish Wish, err error) {
