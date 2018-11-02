29f7b32e6bc1f91ca802f88c7cd9ef38bc732033
Simone Mosciatti <simone.mosciatti@cern.ch>
2018-11-02 17:14:48 +0100

    implement logic to garbage collect a single layer

--- daemon/lib/garbage_collection.go
+++ daemon/lib/garbage_collection.go
@@ -88,9 +88,73 @@ func findImageToGarbageCollect(CVMFSRepo string) ([]da.Manifest, error) {
 	return schedule, nil
 }
 
-// with image and layer we pass the digest of the layer and the digest of the image
+// with image and layer we pass the digest of the layer and the digest of the image, both without the sha256: prefix
 func garbabeCollectSingleLayer(CVMFSRepo, image, layer string) error {
-	return nil
+	backlink, err := getBacklinkFromLayer(CVMFSRepo, layer)
+	llog := func(l *log.Entry) *log.Entry {
+		return l.WithFields(log.Fields{"action": "garbage collect layer",
+			"repo":  CVMFSRepo,
+			"image": image,
+			"layer": layer})
+	}
+	if err != nil {
+		llog(LogE(err)).Error("Impossible to retrieve the backlink information")
+		return err
+	}
+	var newOrigin []string
+	for _, origin := range backlink.Origin {
+		withoutPrefix := strings.Split(origin, ":")[1]
+		if withoutPrefix != image {
+			newOrigin = append(newOrigin, origin)
+		}
+	}
+	if len(newOrigin) > 0 {
+		backlink.Origin = newOrigin
+		backLinkMarshall, err := json.Marshal(backlink)
+		if err != nil {
+			llog(LogE(err)).Error("Error in marshaling the new backlink")
+			return err
+		}
+
+		backlinkPath := getBacklinkPath(CVMFSRepo, layer)
+
+		err = ExecCommand("cvmfs_server", "transaction", CVMFSRepo).Start()
+		if err != nil {
+			llog(LogE(err)).Error("Error in opening the transaction")
+			return err
+		}
+
+		dir := filepath.Dir(backlinkPath)
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			err = os.MkdirAll(dir, 0666)
+			if err != nil {
+				llog(LogE(err)).WithFields(log.Fields{"directory": dir}).Error(
+					"Error in creating the directory for the backlinks file, skipping...")
+				return err
+			}
+		}
+
+		err = ioutil.WriteFile(backlinkPath, backLinkMarshall, 0666)
+		if err != nil {
+			llog(LogE(err)).WithFields(log.Fields{"file": backlinkPath}).Error(
+				"Error in writing the backlink file, skipping...")
+			return err
+		}
+
+		err = ExecCommand("cvmfs_server", "publish", CVMFSRepo).Start()
+		if err != nil {
+			llog(LogE(err)).Error("Error in publishing after adding the backlinks")
+			return err
+		}
+		// write it to file
+		return nil
+	} else {
+		err = RemoveLayer(CVMFSRepo, layer)
+		if err != nil {
+			llog(LogE(err)).Error("Error in deleting the layer")
+		}
+		return err
+	}
 }
 
 func RemoveUselessLayers() error {
